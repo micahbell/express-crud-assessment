85d3681b9a1e8d8b88b860ea1ea4793be77cefe8
Micah Bell <micahmbell@gmail.com>
2015-07-23 14:10:22 -0600

    Second story partially complete.

--- routes/zine.js
+++ routes/zine.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
-// var db = require('monk')('localhost/zine-db');
-// var articles = db.???('articles');
+var db = require('monk')('localhost/zine-db');
+var articles = db.get('articles');
 
 router.get('/zine/index', function(req, res, next) {
-  res.render('zine/index');
+  articles.find({}, function(err, record) {
+    res.render('zine/index', { theArticles: record });
+  });
 });
 
 router.get('/zine/new-article', function(req, res, next) {
   res.render('zine/new-article');
+});
+
+router.post('/zine/index', function(req, res, next) {
+  articles.insert({
+    title: req.body.title,
+    bgURL: req.body.background,
+    bgValue: req.body.background_value,
+    excerpt: req.body.excerpt,
+    body: req.body.body
+  });
+  res.redirect('/zine/index');
 });
 
 
